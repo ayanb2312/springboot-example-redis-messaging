80d84826853c46d1f2f37abf1fe463ea915fb941
Bose <ayan.bose@siemens.com>
2021-02-13 22:52:34 +0530

    first commit

---
+++ src/test/java/com/ab/redis/messaging/springbootexampleredismessaging/SpringbootExampleRedisMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.redis.messaging.springbootexampleredismessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExampleRedisMessagingApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
